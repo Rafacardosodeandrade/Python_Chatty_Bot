dc18a74ebe00623ed2df36baccd17284ef3ed035
Rafael <rafacardosodeandrade@gmail.com>
2020-06-23 14:43:05 -0500

    tip

--- First_Program_Example.py
+++ First_Program_Example.py
@@ -2,4 +2,4 @@ print ('Learn Python to be great!')
 print ('Hello World')
 print ('Python 3.x')
 print ("Yes, I'm ready to learn Python.")
-print ("Alice's Adventures in Wonderland")
+print("Alice's Adventures in Wonderland") #no spaces before the parenthesis
