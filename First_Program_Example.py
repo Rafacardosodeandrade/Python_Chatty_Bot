5299567f22e451633338a6ce76c317761d3c296f
Rafael <rafacardosodeandrade@gmail.com>
2020-06-23 14:54:58 -0500

    a

--- First_Program_Example.py
+++ First_Program_Example.py
@@ -3,3 +3,5 @@ print ('Hello World')
 print ('Python 3.x')
 print ("Yes, I'm ready to learn Python.")
 print("Alice's Adventures in Wonderland") #no spaces before the parenthesis
+print("We learn Python")
+print('2 + 2 = 4')
---
+++ Multi_Line_programs.py
@@ -0,0 +1,9 @@
+# Write a program that prints the following three text lines in a column:
+#
+# first
+# second
+# third
+
+print("first")
+print("second")
+print("third")
