e9452f72bf11f5b4e707dfcad6126424e5664170
Rafael <rafacardosodeandrade@gmail.com>
2020-06-25 09:38:37 -0500

    int and float basic operations

---
+++ Exercises.py
@@ -0,0 +1,4 @@
+print(int(input()) - int(input()))  # print diference between integer numbers.
+
+print(float(input()) + float (input())  # print sum between float numbers.
+
---
+++ Project/2_5.py
@@ -0,0 +1,13 @@
+# #######################################
+# The greater-than symbol followed by space (> ) represents the user input. 
+# Notice that it's not the part of the input.
+#
+# Example 1: a dialogue with the bot
+#
+# Hello! My name is Aid.
+# I was created in 2020.
+# Please, remind me your name.
+# > Max 
+# What a great name you have, Max!
+#
+##########################################
