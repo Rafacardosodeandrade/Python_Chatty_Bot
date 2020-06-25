83f44244896948116fadf71bfeb89b43e4d4fda3
Rafael <rafacardosodeandrade@gmail.com>
2020-06-25 10:01:04 -0500

    exercises variables

--- Exercises.py
+++ Exercises.py
@@ -1,4 +1,24 @@
 print(int(input()) - int(input()))  # print diference between integer numbers.
 
+############
+
 print(float(input()) + float (input())  # print sum between float numbers.
+
+############
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+print(number1 + number2 + number3)  # put your python code here
+
+############
+
+n = int(input())
+word = str(input())
+print(word * n)
+
+############
+
+
+
 
