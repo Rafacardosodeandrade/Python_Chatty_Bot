cc59d1aa1f52d627215f0da9a00b01148742b2b8
Rafael <rafacardosodeandrade@gmail.com>
2020-06-27 10:58:11 -0500

    17/100

--- 3_5_exercises.py
+++ 3_5_exercises.py
@@ -93,3 +93,15 @@ if half1 == half2:
 else:
     print("Ordinary")
 
+##################################################################
+# For the given amount of money, calculate the income from this
+# savings account with a 5% interest rate after one year.
+#
+# Save the result into the variable income.
+#
+# SOLUTION:
+amount = 1000
+interest_rate = 5
+years = 1
+# change the next line
+income = amount * interest_rate / 100
