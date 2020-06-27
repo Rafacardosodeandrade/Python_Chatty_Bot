a008e116668113659a7f0e2a04485560e0831442
Rafael <rafacardosodeandrade@gmail.com>
2020-06-27 10:13:05 -0500

    Progr with numbers

--- 3_5_exercises.py
+++ 3_5_exercises.py
@@ -37,3 +37,20 @@ b = (n // 10) % 10
 c = n % 10
 sum_digit = a + b + c
 print(sum_digit)
+
+
+# Write a program that will help people who are going on vacation. The program should calculate the total required sum (e.g. $) of money to have a good rest for a given duration.
+#
+# There are four parameters which have to be considered:
+#
+# duration in days
+# total food cost per a day
+# one-way flight cost
+# cost of one night in a hotel (the number of nights equal duration minus one)
+# Read integer values of these parameters from the standard input and then print the result.
+#
+# Hint: Do not forget to consider the flight back
+####################################################################################
+
+vac_days, food_cost, flight_cost, stay_cost = [abs(int(input())) for i in range(4)]
+print(food_cost * vac_days + 2 * flight_cost + stay_cost * (vac_days - 1))
